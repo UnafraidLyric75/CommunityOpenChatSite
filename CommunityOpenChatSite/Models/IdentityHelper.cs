7cd6afb5764f5f38cb81e8804582a27f884e8d16
John Uthus <john.a.uthus.jr1@gmail.com>
2020-10-22 20:38:16 -0700

    Created Accounts

--- CommunityOpenChatSite/Models/IdentityHelper.cs
+++ CommunityOpenChatSite/Models/IdentityHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,12 @@ namespace CommunityOpenChatSite.Models
 {
     public static class IdentityHelper
     {
+        // Role names 
+        // Pretender theres a vistor as well here as thats going to be the default
+        // for the website
+        public const string User = "User";
+        public const string Admin = "Admin";
+
         public static void SetIdentityOptions(IdentityOptions options)
         {
             // set sign in options
@@ -24,6 +31,21 @@ namespace CommunityOpenChatSite.Models
             // Set lockout options
             options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
             options.Lockout.MaxFailedAccessAttempts = 3; // defualt is 5
+        }
+
+        public static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        {
+            RoleManager<IdentityRole> roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            // Create role if it does not exist
+            foreach (string role in roles)
+            {
+                bool doesRoleExist = await roleManager.RoleExistsAsync(role);
+                if (!doesRoleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
         }
     }
 }
