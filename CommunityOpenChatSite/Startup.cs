b9cbe1859adaf269ebd7d5b631e11f1adf65ee3a
John Uthus <john.a.uthus.jr1@gmail.com>
2020-10-22 20:34:11 -0700

    Set Up password

--- CommunityOpenChatSite/Startup.cs
+++ CommunityOpenChatSite/Startup.cs
@@ -12,6 +12,7 @@ using CommunityOpenChatSite.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommunityOpenChatSite.Models;
 
 namespace CommunityOpenChatSite
 {
@@ -30,7 +31,7 @@ namespace CommunityOpenChatSite
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(IdentityHelper.SetIdentityOptions)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
