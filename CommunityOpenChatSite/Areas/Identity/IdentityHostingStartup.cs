1743e66fd8ef83e583e46e384626881f36c14da8
John Uthus <john.a.uthus.jr1@gmail.com>
2020-10-22 20:25:10 -0700

    Added identiry files

---
+++ CommunityOpenChatSite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommunityOpenChatSite.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CommunityOpenChatSite.Areas.Identity.IdentityHostingStartup))]
+namespace CommunityOpenChatSite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
