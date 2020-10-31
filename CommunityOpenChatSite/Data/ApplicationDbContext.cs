4ed52a4260e2cd2fb66029f8418090da9c72352c
John Uthus <john.a.uthus.jr1@gmail.com>
2020-10-31 01:06:31 -0700

    Added Photos to DB set

--- CommunityOpenChatSite/Data/ApplicationDbContext.cs
+++ CommunityOpenChatSite/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using CommunityOpenChatSite.Models;
 
 namespace CommunityOpenChatSite.Data
 {
@@ -12,5 +13,7 @@ namespace CommunityOpenChatSite.Data
             : base(options)
         {
         }
+
+        public DbSet<PictureForum> PhotoPosts { get; set; }
     }
 }
