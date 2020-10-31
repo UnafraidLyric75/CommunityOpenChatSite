016f10b504552e3f33debda76487bf057ef2adf8
John Uthus <63675144+UnafraidLyric75@users.noreply.github.com>
2020-10-31 01:09:30 -0700

    Issue#8 create basic text form (#9)
    * Created basic text/photo forum
    
    * Added Photos to DB set
    
    * Added migrations for photot\s and basic text forum

---
+++ CommunityOpenChatSite/Models/TextForum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityOpenChatSite.Models
+{
+    /// <summary>
+    /// A genaric text forum
+    /// </summary>
+    public class TextForum
+    {
+        [Key]
+        public int TextForumId { get; set; }
+
+        public string ForumName { get; set; }
+
+        public string ForumDescription { get; set; }
+    }
+
+    /// <summary>
+    /// A picture forum
+    /// </summary>
+    public class PictureForum : TextForum
+    {
+        /// <summary>
+        /// Embed code for photo
+        /// </summary>
+        public string EmbedCode { get; set; }
+    }
+}
