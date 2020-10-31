78429d1112519fcd439aa83fd8370a5b5e78ced5
John Uthus <john.a.uthus.jr1@gmail.com>
2020-10-31 01:07:53 -0700

    Added migrations for photot\s and basic text forum

---
+++ CommunityOpenChatSite/Data/Migrations/20201031080728_AddPhotoForms.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommunityOpenChatSite.Data.Migrations
+{
+    public partial class AddPhotoForms : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PhotoPosts",
+                columns: table => new
+                {
+                    TextForumId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ForumName = table.Column<string>(nullable: true),
+                    ForumDescription = table.Column<string>(nullable: true),
+                    EmbedCode = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhotoPosts", x => x.TextForumId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhotoPosts");
+        }
+    }
+}
